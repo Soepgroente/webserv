2af66b18f927f2e0e9b2a6730f8637aa64191dbe
Soepgroente <117657433+Soepgroente@users.noreply.github.com>
2025-02-11 13:16:19 +0100

    Fix default error pages

--- src/HttpResponse.cpp
+++ src/HttpResponse.cpp
@@ -3,26 +3,26 @@
 std::map<int, std::string> HttpResponse::defaultResponses =
 {
 	{200, "HTTP/1.1 200 OK\r\nContent-Length: "},
-	{201, "HTTP/1.1 201 Created\r\nContent-Length: "},
-	{204, "HTTP/1.1 204 No Content\r\nContent-Length: "},
+	{201, "HTTP/1.1 201 Created\r\nContent-Type: image/jpeg\r\nContent-Length: "},
+	{204, "HTTP/1.1 204 No Content\r\nContent-Type: image/jpeg\r\nContent-Length: "},
 
-	{301, "HTTP/1.1 301 Moved Permanently\r\nContent-Length: "},
-	{302, "HTTP/1.1 302 Found\r\nContent-Length: "},
-	{304, "HTTP/1.1 304 Not Modified\r\nContent-Length: "},
+	{301, "HTTP/1.1 301 Moved Permanently\r\nContent-Type: image/jpeg\r\nContent-Length: "},
+	{302, "HTTP/1.1 302 Found\r\nContent-Type: image/jpeg\r\nContent-Length: "},
+	{304, "HTTP/1.1 304 Not Modified\r\nContent-Type: image/jpeg\r\nContent-Length: "},
 
-	{400, "HTTP/1.1 400 Bad Request\r\nContent-Length: "},
-	{401, "HTTP/1.1 401 Unauthorized\r\nContent-Length: "},
-	{403, "HTTP/1.1 403 Forbidden\r\nContent-Length: "},
-	{404, "HTTP/1.1 404 Not Found\r\nContent-Length: "},
-	{405, "HTTP/1.1 405 Method Not Allowed\r\nContent-Length: "},
-	{413, "HTTP/1.1 413 Content Too Large\r\nContent-Length: "},
-	{414, "HTTP/1.1 414 URI Too Long\r\nContent-Length: "},
+	{400, "HTTP/1.1 400 Bad Request\r\nContent-Type: image/jpeg\r\nContent-Length: "},
+	{401, "HTTP/1.1 401 Unauthorized\r\nContent-Type: image/jpeg\r\nContent-Length: "},
+	{403, "HTTP/1.1 403 Forbidden\r\nContent-Type: image/jpeg\r\nContent-Length: "},
+	{404, "HTTP/1.1 404 Not Found\r\nContent-Type: image/jpeg\r\nContent-Length: "},
+	{405, "HTTP/1.1 405 Method Not Allowed\r\nContent-Type: image/jpeg\r\nContent-Length: "},
+	{413, "HTTP/1.1 413 Content Too Large\r\nContent-Type: image/jpeg\r\nContent-Length: "},
+	{414, "HTTP/1.1 414 URI Too Long\r\nContent-Type: image/jpeg\r\nContent-Length: "},
 
-	{500, "HTTP/1.1 500 Internal Server Error\r\nContent-Length: "},
-	{501, "HTTP/1.1 501 Not Implemented\r\nContent-Length: "},
-	{502, "HTTP/1.1 502 Bad Gateway\r\nContent-Length: "},
-	{503, "HTTP/1.1 503 Service Unavailable\r\nContent-Length: "},
-	{505, "HTTP/1.1 505 HTTP Version Not Supported\r\nContent-Length: "}
+	{500, "HTTP/1.1 500 Internal Server Error\r\nContent-Type: image/jpeg\r\nContent-Length: "},
+	{501, "HTTP/1.1 501 Not Implemented\r\nContent-Type: image/jpeg\r\nContent-Length: "},
+	{502, "HTTP/1.1 502 Bad Gateway\r\nContent-Type: image/jpeg\r\nContent-Length: "},
+	{503, "HTTP/1.1 503 Service Unavailable\r\nContent-Type: image/jpeg\r\nContent-Length: "},
+	{505, "HTTP/1.1 505 HTTP Version Not Supported\r\nContent-Type: image/jpeg\r\nContent-Length: "}
 };
 
 HttpResponse::HttpResponse(const HttpResponse& other)
